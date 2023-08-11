f3fa122e8a3e2d910392df6aa8e4e8415ce558cc
unknown <devmarcosav@gmail.com>
2023-08-10 21:22:37 -0300

    feat: responsividade mobile

---
+++ src/components/index.jsx
@@ -0,0 +1,34 @@
+import imgSuccess from "../../assets/images/icon-success.svg";
+import "./module.style.css";
+
+export const SubSuccess = ({ email, setSubscribing, setUserEmail }) => {
+    const handleDimiss = () => {
+        setUserEmail("");
+        setSubscribing(false);
+    };
+
+    return (
+        <>
+            <div className="card-subscribing">
+                <section className="section-card">
+                    <div className="container-img-success">
+                        <img src={imgSuccess} alt="" />
+                    </div>
+                    <div className="container-header-success">
+                        <h1>Thanks for subscribing!</h1>
+                    </div>
+                    <div className="container-msg">
+                        <p>
+                            A confirmation email has been sent to{" "}
+                            <strong>{email}</strong>. Please open it and click
+                            the button inside to confirm your subscription.
+                        </p>
+                    </div>
+                    <div className="container-button">
+                        <button onClick={handleDimiss}>Dismiss message</button>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
+};
